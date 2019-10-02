c82cc341856e5a605ce1850beabc2d6011653ba5
张新征 <programer.email@gmail.com>
2019-10-02 19:23:07 +0800

    add vote

--- java-quartz/src/main/java/com/bytebuf/listener/MyTriggerListener.java
+++ java-quartz/src/main/java/com/bytebuf/listener/MyTriggerListener.java
@@ -4,7 +4,7 @@ import org.quartz.JobExecutionContext;
 import org.quartz.Trigger;
 import org.quartz.listeners.TriggerListenerSupport;
 
-import java.time.LocalTime;
+import java.util.Calendar;
 
 /**
  * @author: 张新征
@@ -16,18 +16,31 @@ public class MyTriggerListener extends TriggerListenerSupport {
         return "myTriggerListener";
     }
 
-    @Override
-    public void triggerFired(Trigger trigger, JobExecutionContext context) {
-        super.triggerFired(trigger, context);
-        LocalTime now = LocalTime.now();
-        System.out.println(now.toString() + " Trigger is fired");
-    }
+//    @Override
+//    public void triggerFired(Trigger trigger, JobExecutionContext context) {
+//        super.triggerFired(trigger, context);
+//        LocalTime now = LocalTime.now();
+//        System.out.println(now.toString() + " Trigger is fired");
+//    }
+//
+//    @Override
+//    public void triggerComplete(Trigger trigger, JobExecutionContext context, Trigger.CompletedExecutionInstruction triggerInstructionCode) {
+//        super.triggerComplete(trigger, context, triggerInstructionCode);
+//        String string = triggerInstructionCode.toString();
+//        LocalTime now = LocalTime.now();
+//        System.out.println(now.toString() + " Trigger is completed " + string);
+//    }
 
     @Override
-    public void triggerComplete(Trigger trigger, JobExecutionContext context, Trigger.CompletedExecutionInstruction triggerInstructionCode) {
-        super.triggerComplete(trigger, context, triggerInstructionCode);
-        String string = triggerInstructionCode.toString();
-        LocalTime now = LocalTime.now();
-        System.out.println(now.toString() + " Trigger is completed " + string);
+    public boolean vetoJobExecution(Trigger trigger, JobExecutionContext context) {
+        System.out.println("vetoJobExecution is running");
+        //如果时间是5，15，25，尾数是5的时间点不执行
+        Calendar now = Calendar.getInstance();
+        int second = now.get(Calendar.SECOND);
+        if (second % 10 == 5) {
+            System.out.println("本次 trigger 不触发");
+            return true;
+        }
+        return false;
     }
 }
